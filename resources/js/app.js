0945fd5dd15c7a9e7cee152d46e6029a37c0006a
Rodrigo Arévalo <rodrigo.arevalo4@gmail.com>
2022-10-19 16:10:53 -0600

    Implementación de alertas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
-
+import 'animate.css';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+function closeAlert() {
+    document.getElementById('alert').classList.add('animate__animated', 'animate__fadeOutRight');
+    setTimeout(function() {
+        // Remove the element from the DOM
+        document.getElementById('alert').remove();
+    }, 1000);
+}
+
+window.closeAlert = closeAlert;
