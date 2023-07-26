f4748a3dc748424577a7fa83312ed882c7f4fbc9
Rodrigo Arévalo <rodrigo.arevalo4@gmail.com>
2023-07-26 02:23:18 -0600

    Acerca de y SEO

--- app/Http/Controllers/PublicPageController.php
+++ app/Http/Controllers/PublicPageController.php
@@ -8,9 +8,20 @@ use Artesaos\SEOTools\Facades\SEOTools;
 class PublicPageController extends Controller
 {
     public function index(){
-
-        SEOTools::setTitle('Inicio');
+        SEOTools::setTitle('Padiush | La Herramienta Definitiva para Investigaciones Etnobotánicas');
+        SEOTools::setDescription('Simplifica la recolección y el análisis de tus datos con nuestra plataforma intuitiva y personalizable.');
+        SEOTools::opengraph()->setUrl(env('APP_URL'));
+        SEOTools::setCanonical(env('APP_URL'));
 
         return view('public.index');
+    }
+
+    public function about(){
+        SEOTools::setTitle('Sobre nosotros');
+        SEOTools::setDescription('Conoce más sobre el equipo detrás de Padiush.');
+        SEOTools::opengraph()->setUrl(env('APP_URL').'/acerca');
+        SEOTools::setCanonical(env('APP_URL').'/acerca');
+
+        return view('public.about');
     }
 }
