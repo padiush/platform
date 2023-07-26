5d2879c23bd3635f80585e03ad35518c68b3e53e
Rodrigo Arévalo <rodrigo.arevalo4@gmail.com>
2023-07-26 16:46:22 -0600

    Sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+use Illuminate\Support\Facades\Log;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a sitemap for the application.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+
+        Log::info('Sitemap generated.');
+    }
+}
