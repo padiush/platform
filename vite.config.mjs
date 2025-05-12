2e0e7f420c034e96eed29f9dc4d8862fa5476a40
Rodrigo Arévalo <rodrigo.arevalo4@gmail.com>
2025-05-11 23:02:25 -0600

    chore: Add linter and new dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.jsx",
-                "resources/js/legacy.js",
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+                'resources/js/legacy.js',
             ],
             refresh: true,
         }),
