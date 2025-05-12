4a40d08da343181686c6d2d183c9005d55ab4362
Rodrigo Arévalo <rodrigo.arevalo4@gmail.com>
2025-05-11 23:05:22 -0600

    fix: Corrections on HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -13,43 +13,45 @@ use Tighten\Ziggy\Ziggy;
  */
 class HandleInertiaRequests extends Middleware
 {
-  /**
-   * The root template that is loaded on the first page visit.
-   *
-   * @var string
-   */
-  protected $rootView = "app";
+    /**
+     * The root template that is loaded on the first page visit.
+     *
+     * @var string
+     */
+    protected $rootView = 'app';
 
-  /**
-   * Determine the current asset version.
-   */
-  public function version(Request $request): string|null
-  {
-    return parent::version($request);
-  }
+    /**
+     * Determine the current asset version.
+     */
+    public function version(Request $request): string|null
+    {
+        return parent::version($request);
+    }
 
-  /**
-   * Define the props that are shared by default.
-   *
-   * @return array<string, mixed>
-   */
-  public function share(Request $request): array
-  {
-    return [
-      ...parent::share($request),
-      "auth" => [
-        "user" => $request->user(),
-        "projects" => $request->user() ? $request->user()->projects->count() : []
-      ],
-      "ziggy" => fn() => [
-        ...(new Ziggy())->toArray(),
-        "location" => $request->url(),
-      ],
-      "flash" => [
-        "message" => $request->session()->get("message"),
-        "message_type" => $request->session()->get("message_type"),
-      ],
-      "environment" => config("app.env"),
-    ];
-  }
+    /**
+     * Define the props that are shared by default.
+     *
+     * @return array<string, mixed>
+     */
+    public function share(Request $request): array
+    {
+        return [
+            ...parent::share($request),
+            'auth' => [
+                'user' => $request->user(),
+                'projects' => $request->user()
+                    ? $request->user()->projects->count()
+                    : [],
+            ],
+            'ziggy' => fn() => [
+                ...(new Ziggy())->toArray(),
+                'location' => $request->url(),
+            ],
+            'flash' => [
+                'message' => $request->session()->get('message'),
+                'message_type' => $request->session()->get('message_type'),
+            ],
+            'environment' => config('app.env'),
+        ];
+    }
 }
