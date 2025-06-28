ebb0b68b0df08e1a4e3ee1fdcd22d04db1fccab8
Rodrigo Arévalo <33340107+raarevalo96@users.noreply.github.com>
2025-06-28 16:02:44 -0600

    Add honeypot protection to registration

--- routes/auth.php
+++ routes/auth.php
@@ -15,7 +15,8 @@ Route::middleware('guest')->group(function () {
         Route::get('register', [RegisteredUserController::class, 'create'])
                     ->name('register');
     
-        Route::post('register', [RegisteredUserController::class, 'store']);
+        Route::post('register', [RegisteredUserController::class, 'store'])
+                    ->middleware(['honeypot']);
     } else {
         Route::get('register', function(){
             return redirect()->route('login');
